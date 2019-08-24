d5d1f2f3de0d6cc22242ff27e33b93ce258902e2
frkhit <frkhit@gmail.com>
2019-08-24 18:25:56 +0800

    clean code

--- tensorflow/tensorflow-object-detection-server/server.py
+++ tensorflow/tensorflow-object-detection-server/server.py
@@ -1,43 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-import shutil
-import uuid
 
-from flask import Flask, request, make_response, Response, redirect, send_file
+from io import BytesIO
 
 import numpy as np
-import os
-import tarfile
+import requests
 import tensorflow as tf
-
 from PIL import Image
-
+from flask import Flask, request, make_response
 from object_detection.utils import label_map_util
-
 from object_detection.utils import visualization_utils as vis_util
 
 
 class ObjectDetetion(object):
-    # What model to download.
-    MODEL_NAME = 'faster_rcnn_nas_coco_2017_11_08'
-    MODEL_FILE = '/root/models/faster_rcnn_nas_coco_2017_11_08.tar.gz'
-    DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
-
     # Path to frozen detection graph. This is the actual model that is used for the object detection.
-    PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
+    PATH_TO_CKPT = '/data/frozen_inference_graph.pb'
 
     # List of the strings that is used to add correct label for each box.
-    PATH_TO_LABELS = os.path.join('/root/servers/data', 'mscoco_label_map.pbtxt')
+    PATH_TO_LABELS = '/tensorflow/models/research/object_detection/data/mscoco_label_map.pbtxt'
 
     NUM_CLASSES = 90
 
     def __init__(self):
-        tar_file = tarfile.open(self.MODEL_FILE)
-        for file in tar_file.getmembers():
-            file_name = os.path.basename(file.name)
-            if 'frozen_inference_graph.pb' in file_name:
-                tar_file.extract(file, os.getcwd())
-
         detection_graph = tf.Graph()
         with detection_graph.as_default():
             od_graph_def = tf.GraphDef()
@@ -95,68 +79,30 @@ class ObjectDetetion(object):
         return image_np
 
 
-def create_guid():
-    return str(uuid.uuid1()).lower()
-
-
-def get_md5(string):
-    import hashlib
-    hash_md5 = hashlib.md5(string.encode('utf-8'))
-    return hash_md5.hexdigest()
-
-
-# 实例化Flask服务器
 app = Flask(__name__)
-manager = None
-
-
-class UrlToImageFile(object):
-    def __init__(self):
-        import requests
-        self.client = requests
-
-    def save_large_file(self, url, file_name):
-        r = self.client.get(url, stream=True)
-        if r.status_code == 200:
-            with open(file_name, 'wb') as f:
-                r.raw.decode_content = True
-                shutil.copyfileobj(r.raw, f)
-
-    def save_file(self, url, file_name):
-        r = self.client.get(url, stream=True)
-        if r.status_code == 200:
-            with open(file_name, 'wb') as f:
-                f.write(r.content)
-
-
-def save_image(image_np):
-    try:
-        _id = get_md5(create_guid())
-        image_id_ = "/root/servers/" + _id + ".jpg"
-        im = Image.fromarray(image_np)
-        im.save(image_id_)
-        return send_file(image_id_, mimetype='image/jpeg')
-    except Exception as e:
-        return str(e), 404
-    except:
-        return "XXXX", 404
-
-
 api_obj = ObjectDetetion()
 
 
-@app.route("/api", methods=["POST", 'GET'])
+@app.route("/", methods=["POST", 'GET'])
 def search_image():
     url = request.args.get("url") or request.form.get("url")
     if not url:
         response = make_response("OK", 200)
     else:
         try:
-            tmp_file_name = create_guid() + ".jpg"
-            UrlToImageFile().save_file(url, tmp_file_name)
-            image = Image.open(tmp_file_name)
-            image_np = api_obj.api(image)
-            return save_image(image_np)
+            # load image
+            raw_image_response = requests.get(url=url, timeout=10)
+
+            # object detection
+            image = Image.open(BytesIO(raw_image_response.content))
+            image_np = api_obj.api(image=image)
+            new_image_obj = Image.fromarray(image_np)
+
+            # response
+            new_image_bytes = BytesIO()
+            new_image_obj.save(new_image_bytes, 'JPEG')
+            response = make_response(new_image_bytes.getvalue())
+            response.headers['Content-Type'] = "image/jpeg"
         except Exception as e:
             print(e)
             response = make_response("error {}".format(e), 200)
