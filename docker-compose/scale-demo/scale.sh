3ac08675645be8426a7800a6a7475efd6287a10a
frkhit <frkhit@gmail.com>
2020-03-09 23:44:56 +0800

    Update scale.sh

--- docker-compose/scale-demo/scale.sh
+++ docker-compose/scale-demo/scale.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-for i in $(seq 1 10);
-  do SERVER_INDEX=$i docker-compose up -d --scale py-server=$i --no-recreate;
+for i in $(seq 1 10); do 
+  SERVER_INDEX=$i docker-compose up -d --scale py-server=$i --no-recreate;
+  # if not work, use command below
+  # echo "SERVER_INDEX=${i}" > .env && SERVER_INDEX=$i docker-compose up -d --scale py-server=$i --no-recreate
 done
